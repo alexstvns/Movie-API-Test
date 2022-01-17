2226a88d9b18ce6ea407148d6178f428575f0b7e
alexstvns <alexanderlstevens@gmail.com>
2022-01-17 13:50:15 -0800

    V1.17.22.149
    Added additional random card generator functions.
    Added result clearing logic on each search.
    Added ability to input movie poster link to result card builder.

--- MovieSearch.js
+++ MovieSearch.js
@@ -1,14 +1,23 @@
-
-
+let rMax = Math.random() *10 +1;
 
 function SearchFunction (){
 
 let SearchResults = document.querySelector('.Results-Container');
 
+//clear results and then create random max count.
+if(SearchResults.firstChild){
+    while(SearchResults.firstChild){
+        SearchResults.removeChild(SearchResults.firstChild);
+    }
+    rMax = Math.random() *10 +1;
+}
+
+
+  
   var results = [];
   // For testing, creates 10  result card elements and adds them to
   // the results container  
-  for (var i = 0; i < 10; i++){
+  for (var i = 0; i < rMax; i++){
       results.push(DemoResults());
   }
 
@@ -19,18 +28,19 @@ let SearchResults = document.querySelector('.Results-Container');
     SearchResults.append(element);
   });
   
-
-
+ //let ShowOff = ResultBuilder('https://flxt.tmsimg.com/assets/p13606_p_v10_am.jpg','Hook','A movie about an adult Peter Pan, and adults finding happy thoughts');
+ //SearchResults.append(ShowOff);
 
 }
 
 
-function ResultBuilder(heading, para){
+function ResultBuilder(img, heading, para){
 
     var resultNode = document.createElement("div");
     resultNode.classList.add('Result-Card');
     var imgNode = document.createElement('div');
     imgNode.classList.add('Result-Image');
+    imgNode.style.backgroundImage = `url(${img})`;
     var headerNode = document.createElement('h2');
     var paraNode = document.createElement('p');
 
@@ -53,7 +63,8 @@ function ResultBuilder(heading, para){
 
 function DemoResults() {
     // call this function when you need to test card creation/population. 
-    let demoResult = ResultBuilder('Movie Title Is Here', 'This is where the data goes here for the movies')
+    let demoResult = ResultBuilder('./img/camera-roll.jpg', 'Movie Title Is Here', 'This is where the data goes for the movies once the API is hooked up')
     console.log(demoResult);
     return demoResult;
 }
+
